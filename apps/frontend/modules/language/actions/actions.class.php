1b7d42c8e92b8c6cb5073a3ef2bc31793ce89ede
Abdul Sammi Gul <sammigul11005@gmail.com>
2024-03-11 10:06:33 +0500

    added actions for changeLanguage

---
+++ apps/frontend/modules/language/actions/actions.class.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * language actions.
+ *
+ * @package    jobeet
+ * @subpackage language
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class languageActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+
+  /**
+   * Executes changeLanguage action
+   *  @param sfWebRequest $request A request object
+   *  
+   */
+  public function executeChangeLanguage(sfWebRequest $request)
+  {
+    $form = new sfFormLanguage(
+      $this->getUser(),
+      array('languages' => array('en', 'fr'))
+    );
+ 
+    $form->process($request);
+ 
+    return $this->redirect('localized_homepage');
+  }
+}
